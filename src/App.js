357c486f96b5c4fbf30d35b25722b8679f8681c1
unknown <ugborjohn@gmai.com>
2020-09-05 15:18:55 +0100

    added menubar to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <MenuAppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
