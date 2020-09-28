fe9eeb399f2d1bcaf52f67010eaf8f3ebf34e68e
Taro <aztaro97@gmail.com>
2020-09-28 13:24:48 +0100

    Push the code

--- src/components/contributorDashboard.js
+++ src/components/contributorDashboard.js
@@ -4,36 +4,39 @@ import "../dashboard.css";
 
 function AsideNav() {
   return (
-    <nav className="asideNav col-3 px-5 pt-3" style={{ background: "#344127" }}>
-      <div className="container text-left">
-        <a className="h2 text-white Aside-title" href="/">
+    <nav
+      classNameName="asideNav col-3 px-5 pt-3"
+      style={{ background: "#344127" }}
+    >
+      <div classNameName="container text-left">
+        <a classNameName="h2 text-white Aside-title" href="/">
           <img src="images/onboard_menu_icon.svg" alt="" /> Open Data
         </a>
 
-        <ul className="navbar-nav mt-5 ">
-          <li className="nav-item active">
-            <a href="/" className="nav-link h4">
-              <i className="fas fa-home mr-2"></i> Dashboard
+        <ul classNameName="navbar-nav mt-5 ">
+          <li classNameName="nav-item active">
+            <a href="/" classNameName="nav-link h4">
+              <i classNameName="fas fa-home mr-2"></i> Dashboard
             </a>
           </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link h4">
-              <i className="fas fa-chart-line mr-2"></i> Stats
+          <li classNameName="nav-item">
+            <a href="/" classNameName="nav-link h4">
+              <i classNameName="fas fa-chart-line mr-2"></i> Stats
             </a>
           </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link h4">
-              <i className="fas fa-info-circle mr-2"></i> Information
+          <li classNameName="nav-item">
+            <a href="/" classNameName="nav-link h4">
+              <i classNameName="fas fa-info-circle mr-2"></i> Information
             </a>
           </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link h4">
-              <i className="fas fa-cloud-upload-alt mr-2"></i> Uploads
+          <li classNameName="nav-item">
+            <a href="/" classNameName="nav-link h4">
+              <i classNameName="fas fa-cloud-upload-alt mr-2"></i> Uploads
             </a>
           </li>
-          <li className="nav-item">
-            <a href="/" className="nav-link h4">
-              <i className="far fa-comment-alt mr-2"></i> Contact Us
+          <li classNameName="nav-item">
+            <a href="/" classNameName="nav-link h4">
+              <i classNameName="far fa-comment-alt mr-2"></i> Contact Us
             </a>
           </li>
         </ul>
@@ -44,31 +47,31 @@ function AsideNav() {
 
 function Container() {
   return (
-    <main class="col-9">
-      <div class="container">
-        <header class="container d-flex justify-content-between px-0 pt-3">
+    <main className="col-9">
+      <div className="container">
+        <header className="container d-flex justify-content-between px-0 pt-3">
           <div>
-            <a href="/" class="nav-left nav-link text-white">
-              <i class="fas fa-upload mr-2"></i>Upload Dataset
+            <a href="/" className="nav-left nav-link text-white">
+              <i className="fas fa-upload mr-2"></i>Upload Dataset
             </a>
           </div>
-          <ul class="nav-right d-flex navbar-nav flex-row">
-            <li class="nav-item px-3">
-              <i class="far fa-bell"></i> 2
+          <ul className="nav-right d-flex navbar-nav flex-row">
+            <li className="nav-item px-3">
+              <i className="far fa-bell"></i> 2
             </li>
             <li>
               <a
-                className="border-dark btn btn-outline-light text-dark px-4 py-0"
+                classNameName="border-dark btn btn-outline-light text-dark px-4 py-0"
                 href="/"
               >
                 Level 1
               </a>
             </li>
 
-            <li class="nav-item px-3">
-              <a href="/" class="nav-link py-0">
+            <li className="nav-item px-3">
+              <a href="/" className="nav-link py-0">
                 <img
-                  class="img-fluid rounded-circle"
+                  className="img-fluid rounded-circle"
                   width="20"
                   height="10"
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRMdVmNPFIXw0viVx_1ikpgYWNaEvKVPdKcg&usqp=CAU"
@@ -78,9 +81,9 @@ function Container() {
               </a>
             </li>
 
-            <li class="dropdown show nav-item px-3">
+            <li className="dropdown show nav-item px-3">
               <a
-                class="dropdown-toggle"
+                className="dropdown-toggle"
                 href="/"
                 role="button"
                 id="dropdownMenuLink"
@@ -89,24 +92,27 @@ function Container() {
                 aria-expanded="false"
               ></a>
 
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="/">
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+              >
+                <a className="dropdown-item" href="/">
                   Action
                 </a>
-                <a class="dropdown-item" href="/">
+                <a className="dropdown-item" href="/">
                   Another action
                 </a>
-                <a class="dropdown-item" href="/">
+                <a className="dropdown-item" href="/">
                   Something else here
                 </a>
               </div>
             </li>
           </ul>
         </header>
-        <section class="mt-5">
-          <div class="card" style={{ background: "#344127" }}>
-            <div class="card-body text-white">
-              <h5 class="font-weight-normal">
+        <section className="mt-5">
+          <div className="card" style={{ background: "#344127" }}>
+            <div className="card-body text-white">
+              <h5 className="font-weight-normal">
                 Welcome John Vibe <br />
                 <br />
                 We are glad to have you as a contributor to Open Data for Africa
@@ -115,181 +121,187 @@ function Container() {
                 <br />
                 <br />
                 If you have any abouts how to upload your first dataset,
-                <a class="d-inline btn_ClickHere" href="/">
+                <a className="d-inline btn_ClickHere" href="/">
                   click here.
                 </a>
               </h5>
             </div>
           </div>
         </section>
-        <section class="mt-5 text-white OdaSection">
+        <section className="mt-5 text-white OdaSection">
           <h1 style={{ color: "#344127" }}>Trending Categories</h1>
-          <h5 class="py-2" style={{ color: "#344127" }}>
+          <h5 className="py-2" style={{ color: "#344127" }}>
             Our recommendations of datasets that are getting most searches...
           </h5>
 
           <div
             id="recipeCarousel"
-            class="carousel slide w-100"
+            className="carousel slide w-100"
             data-ride="carousel"
           >
-            <div class="carousel-inner w-100" role="listbox">
-              <div class="carousel-item row no-gutters active">
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+            <div className="carousel-inner w-100" role="listbox">
+              <div className="carousel-item row no-gutters active">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/Doctor-amico.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">
+                      <h5 className="card-title text-white text-center">
                         Health Care
                       </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/Farmer-amico.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">
+                      <h5 className="card-title text-white text-center">
                         Agriculture
                       </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/House_searching-bro.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">Housing</h5>
+                      <h5 className="card-title text-white text-center">
+                        Housing
+                      </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/Farmer-amico.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">
+                      <h5 className="card-title text-white text-center">
                         Agriculture
                       </h5>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="carousel-item row no-gutters">
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+              <div className="carousel-item row no-gutters">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/Doctor-amico.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">
+                      <h5 className="card-title text-white text-center">
                         Health Care
                       </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/House_searching-bro.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">Housing</h5>
+                      <h5 className="card-title text-white text-center">
+                        Housing
+                      </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/Farmer-amico.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">
+                      <h5 className="card-title text-white text-center">
                         Agriculture
                       </h5>
                     </div>
                   </div>
                 </div>
-                <div class="col-3 float-left">
-                  <div class="card mb-2 mx-3">
-                    <div class="card-body">
+                <div className="col-3 float-left">
+                  <div className="card mb-2 mx-3">
+                    <div className="card-body">
                       <img
                         src="images/dashboard-svg/House_searching-bro.svg"
                         alt=""
-                        class="card-img-top"
+                        className="card-img-top"
                       />
-                      <h5 class="card-title text-white text-center">Housing</h5>
+                      <h5 className="card-title text-white text-center">
+                        Housing
+                      </h5>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
             <a
-              class="carousel-control-prev"
+              className="carousel-control-prev"
               href="#recipeCarousel"
               role="button"
               data-slide="prev"
             >
               <span
-                class="carousel-control-prev-icon"
+                className="carousel-control-prev-icon"
                 aria-hidden="true"
               ></span>
-              <span class="sr-only">Previous</span>
+              <span className="sr-only">Previous</span>
             </a>
             <a
-              class="carousel-control-next"
+              className="carousel-control-next"
               href="#recipeCarousel"
               role="button"
               data-slide="next"
             >
               <span
-                class="carousel-control-next-icon"
+                className="carousel-control-next-icon"
                 aria-hidden="true"
               ></span>
-              <span class="sr-only">Next</span>
+              <span className="sr-only">Next</span>
             </a>
           </div>
         </section>
 
-        <section class="my-5">
+        <section className="my-5">
           <h1 style={{ color: "#344127" }}>ODA Digest</h1>
           <div
-            class="card py-5 mt-5"
+            className="card py-5 mt-5"
             style={{
               background:
                 "linear-gradient(178.18deg,rgba(249, 212, 142, 0.9) -13.56%,rgba(52, 65, 39, 0.9) -13.56%,rgba(249, 212, 142, 0.9) 158.3%)",
             }}
           >
-            <div class="card-body text-white text-center">
-              <h5 class="h4">
+            <div className="card-body text-white text-center">
+              <h5 className="h4">
                 We have decided to share our stories to the world , join us on
                 June 01 2020 as we launch the converse conversation with Joe
                 Rogan and peter Thiel.
               </h5>
               <button
                 type="button"
-                class="btn btn-outline-light mt-3 text-white"
+                className="btn btn-outline-light mt-3 text-white"
                 style={{ fontSize: "25px" }}
               >
                 Learn More
@@ -304,7 +316,7 @@ function Container() {
 
 function Dashboard() {
   return (
-    <div className="row">
+    <div classNameName="row">
       <AsideNav />
       <Container />
     </div>
