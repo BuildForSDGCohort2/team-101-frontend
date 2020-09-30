6cee14fa82191ce286a6af507c1560d2d74e55ad
Taro <aztaro97@gmail.com>
2020-09-30 12:19:11 +0100

    Add the dropDown toggle button

--- src/components/contributorDashboard.js
+++ src/components/contributorDashboard.js
@@ -65,40 +65,38 @@ function Container() {
               </a>
             </li>
 
-            <li class="nav-item px-3">
-              <a href="/" class="nav-link py-0">
+            <li class="nav-item pl-3 pr-1">
+              <a href="/" class="nav-link text-dark py-0">
                 <img
-                  class="img-fluid rounded-circle"
-                  width="20"
-                  height="10"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRMdVmNPFIXw0viVx_1ikpgYWNaEvKVPdKcg&usqp=CAU"
+                  class="img-fluid rounded-circle pr-2"
+                  width="30"
+                  height="30"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUwjB_JQ42cHVQHw6eWFp882_4okQmwCJxPA&usqp=CAU"
                   alt=""
                 />
                 Johnvibe104
               </a>
             </li>
 
-            <li class="dropdown show nav-item px-3">
-              <a
-                class="dropdown-toggle"
-                href="/"
-                role="button"
-                id="dropdownMenuLink"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              ></a>
-
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="/">
-                  Action
-                </a>
-                <a class="dropdown-item" href="/">
-                  Another action
-                </a>
-                <a class="dropdown-item" href="/">
-                  Something else here
-                </a>
+            <li class="nav-item pr-3">
+              <div class="dropdown mr-1">
+                <button
+                  type="button"
+                  class="dropdown-toggle border-0 bg-transparent"
+                  id="dropdownMenuOffset"
+                  data-toggle="dropdown"
+                  data-offset="10,20"
+                >
+                  
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                  <a class="dropdown-item" href="/">
+                    Log in
+                  </a>
+                  <a class="dropdown-item" href="/">
+                    Sign Up
+                  </a>
+                </div>
               </div>
             </li>
           </ul>
